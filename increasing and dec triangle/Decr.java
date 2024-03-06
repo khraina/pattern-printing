4ec66fcd1816341fd5bf49f4a53629d6679084a5
khraina <84083826+khraina@users.noreply.github.com>
2024-03-06 20:53:05 +0530

    basics

---
+++ increasing and dec triangle/Decr.java
@@ -0,0 +1,26 @@
+class Decr{
+    public static void main(String[] args) {
+        //increasing triangle 
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print("$");
+            }
+
+            System.out.println();
+        }
+    
+    }
+}
+
+/*
+ * output
+ 
+$$$$$
+$$$$
+$$$
+$$
+$
+
+
+ */
