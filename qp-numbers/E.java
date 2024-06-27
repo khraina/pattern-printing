9826cf264e18323a68f0fb5a788fc3147821a50f
raina kh <84083826+khraina@users.noreply.github.com>
2024-06-27 07:53:58 +0000

    coloum order

---
+++ qp-numbers/E.java
@@ -0,0 +1,32 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int n=5;
+	    
+		
+		for(int i=1;i<=n;i++)
+		{   int p=1;
+		    for(int j=1;j<=i;j++)         
+		    {   
+		       
+		        System.out.print(p++ + " ");
+		    } 
+		 System.out.println();
+		   
+	}
+}
+}
+
+/*output  
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+
+
+*/
